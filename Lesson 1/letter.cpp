b1c5134c14ddedf9f2118f549a5a571bf6023f07
Matthew Lanier <speclabsmatt@gmail.com>
2024-08-24 09:45:59 +0700

    Initial commit : Lesson 1

---
+++ Lesson 1/letter.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+
+  std::cout << "Dear Self,\n";
+  std::cout << "You have been learning web development for more than 10 years. Now it is time to try something a little different.\n";
+  std::cout << "Your goal is to learn C++ for Arduino programming, as well as to learn more about memory management in code.\n";
+  std::cout << "The date is 8/21/2024. Signed: Matthew Lanier\n";
+}
