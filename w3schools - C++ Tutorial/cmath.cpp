f89d414f9d6d182a6a2cd110128d7de9da95cd98
Matthew Lanier <speclabsmatt@gmail.com>
2024-08-31 10:54:21 +0700

    Add cmath example

---
+++ w3schools - C++ Tutorial/cmath.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "Square root of 64: " << std::sqrt(64) << std::endl;
+    std::cout << "Round 2.6: " << std::round(2.6) << std::endl;
+    std::cout << "The natural logarithm of 2 is: " << std::log(10000) << "\n";
+
+    return 0;
+}
