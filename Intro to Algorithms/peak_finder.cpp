425287f5512e38d9f8ae4ac4c0ed45f2febc8f4c
Matthew Lanier <speclabsmatt@gmail.com>
2024-09-03 16:30:30 +0700

    Clean up formatting

--- Intro to Algorithms/peak_finder.cpp
+++ Intro to Algorithms/peak_finder.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> getPeaks(std::vector<int>& points) {
-    std::vector<int> peaks;
+std::vector<int> getPeaks(std::vector<int> &points)
+{
+  std::vector<int> peaks;
 
-    for (int i = 1; i < points.size()- 1; i++) {
-        if (points[i] > points[i + 1] && points[i] > points[i - 1]) {
-            peaks.push_back(points[i]);
-        }
+  for (int i = 1; i < points.size() - 1; i++)
+  {
+    if (points[i] > points[i + 1] && points[i] > points[i - 1])
+    {
+      peaks.push_back(points[i]);
     }
+  }
 
-    return peaks;
+  return peaks;
 }
 
-int main() {
-    std::vector<int> points = { 1, 2, 3, 2, 4, 1, 5, 3 };
-    std::vector<int> peaks = getPeaks(points);
+int main()
+{
+  std::vector<int> points = {1, 2, 3, 2, 4, 1, 5, 3};
+  std::vector<int> peaks = getPeaks(points);
 
-    for (const int i : peaks) {
-        std::cout << i;
-    }
+  for (const int i : peaks)
+  {
+    std::cout << i;
+  }
 }
